68a84a3dcd637e3354cb03ed214b2b27125bf5b3
junodavid121 <55053863+junodavid121@users.noreply.github.com>
2020-01-25 21:40:52 +1100

    New Start

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Hide
+
+function toggleTIme(){
+    document.querySelector(#content).classList.toggle("hide")
+}
+
+
+/* Plain javascript examples */
+
+// var nameElement = document.getElementById('board-content');
+
+// function printHello(name) {
+//     console.log('Hello ' + name);
+// }
+
+// setTimeout(() => {
+//     nameElement.innerHTML = 'Hi';
+// }, 2000);
+
+// var goBtn = document.getElementById('go-btn');
+
+// goBtn.addEventListener('click', () => {
+//     nameElement.innerHTML = 'Go';
+// });
+
+// var nameElement = jQuery('#board-content');
+// nameElement.innerHTML = 'Hi';
+
+/* jQuery examples*/
+
+// var goBtn = $('#go-btn');
+
+// goBtn.click(() => {
+//     updateName('Phoenix');
+// });
+
+// var updateName = name => {
+//     $('#name-content').html('I am ' + name);
+// };
+
+// var updateName = function(name) {
+//     $('#name-content').html('I am ' + name);
+// };
